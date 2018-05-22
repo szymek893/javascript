7140840b9a59b8059396dc1b7ca838368066ecd0
Szymon <szymek893@gmail.com>
2018-05-22 13:43:23 +0200

    'elseiify'

--- qwerty.js
+++ qwerty.js
@@ -1,5 +1,13 @@
-var a = prompt('podaj pierwszą liczbę');
-var h = prompt('podaj drugą liczbę');
-var triangleArea = a*h/2;
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-document.write(triangleArea);
+var a = prompt('wpisz pierwszą liczbę');
+var b = prompt('wpisz drugą liczbę');
+var value = ((a * a) - (2 * a * b) + (b * b));
+console.log('wynik:' + value);
+
+if(value<0){
+	console.log('wynik ujemny');
+}else if(value>0){
+	console.log('wynik dodatni');
+}else{
+	console.log('wynik równy 0');
+}
+
