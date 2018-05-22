bde51e2089789a5004801b6977b1fece06881bfd
Szymon <szymek893@gmail.com>
2018-05-22 13:12:00 +0200

    'dzialanie'

--- qwerty.js
+++ qwerty.js
@@ -1,4 +1,5 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name); 
-
+var a = prompt('podaj pierwszą liczbę');
+var h = prompt('podaj drugą liczbę');
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+document.write(triangleArea);
