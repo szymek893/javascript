05fb64b7bf452275a24f263871779556c8f8a766
Szymon <szymek893@gmail.com>
2018-05-23 09:54:12 +0200

    'zadanie'

--- qwerty.js
+++ qwerty.js
@@ -1,13 +1,10 @@
-var a = prompt('wpisz pierwszą liczbę');
-var b = prompt('wpisz drugą liczbę');
-var value = ((a * a) - (2 * a * b) + (b * b));
-console.log('wynik:' + value);
-
-if(value<0){
-	console.log('wynik ujemny');
-}else if(value>0){
-	console.log('wynik dodatni');
-}else{
-	console.log('wynik równy 0');
+function getTriangleArea(a,h){
+	if((a>0) && (h>0)){
+		return a * h / 2;
+	}else{
+		alert('Nieprawidłowe dane');
+	}
 }
+
+console.log(getTriangleArea(prompt('Wpisz a'),prompt('Wpisz h')));
 
